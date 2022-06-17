be6b5418284d1e5d90174db2ef8c04deef4afc11
Nikita Chabaniuk <92546152+chabanyknikita@users.noreply.github.com>
2022-06-17 17:22:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,64 @@
-from Crypto.PublicKey import RSA
-from Crypto.Random import get_random_bytes
-from Crypto.Cipher import AES, PKCS1_OAEP
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
 
 
-def generate_public_and_private_key():
-    key = RSA.generate(2048)
-    private_key = key.export_key()
-    file_out = open("private_key.pem", "wb")
-    file_out.write(private_key)
-    file_out.close()
-
-    public_key = key.publickey().export_key()
-    file_out = open("public_key.pem", "wb")
-    file_out.write(public_key)
-    file_out.close()
-
-
-a = input("Write text, which you want to encrypt: ")
-
-clear = lambda: print('\n' * 100)
-clear()
+def extendGcd(a, b):
+    if b == 0:
+        x = 1
+        y = 0
+        return x, y
+    else:
+        x1, y1 = extendGcd(b, a % b)
+        x = y1
+        y = x1 - (int)(a / b) * y1
+        return x, y
 
 
-def encrypted_data_rsa():
-    data = a.encode("utf-8")
-    file_out = open("encrypted_data.bin", "wb")
-
-    recipient_key = RSA.import_key(open("public_key.pem").read())
-    session_key = get_random_bytes(16)
-
-    # Encrypt the session key with the public RSA key
-    cipher_rsa = PKCS1_OAEP.new(recipient_key)
-    enc_session_key = cipher_rsa.encrypt(session_key)
-
-    # Encrypt the data with the AES session key
-    cipher_aes = AES.new(session_key, AES.MODE_EAX)
-    ciphertext, tag = cipher_aes.encrypt_and_digest(data)
-    [file_out.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, ciphertext)]
-    file_out.close()
+def fastMul(a, b, n):
+    res = 1
+    while b != 0:
+        if b % 2 == 0:
+            b = b / 2
+            a = (a * a) % n
+        elif b % 2 != 0:
+            b = b - 1
+            res = (res * a) % n
+    return res
 
 
-def decrypted_data_rsa():
-    file_in = open("encrypted_data.bin", "rb")
-
-    private_key = RSA.import_key(open("private_key.pem").read())
-
-    enc_session_key, nonce, tag, ciphertext = \
-        [file_in.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1)]
-
-    # Decrypt the session key with the private RSA key
-    cipher_rsa = PKCS1_OAEP.new(private_key)
-    session_key = cipher_rsa.decrypt(enc_session_key)
-
-    # Decrypt the data with the AES session key
-    cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
-    data = cipher_aes.decrypt_and_verify(ciphertext, tag)
-    print(data.decode("utf-8"))
+def generateKey(p, q):
+    n = p * q
+    fn = (p - 1) * (q - 1)
+    e = 7
+    x, y = extendGcd(e, fn)
+    if x < 0:
+        x = x + fn
+    d = x
+    return (n, e), (n, d)
 
 
-generate_public_and_private_key()
-encrypted_data_rsa()
-decrypted_data_rsa()
+def encrypt(m, publicKey):
+    n = publicKey[0]
+    e = publicKey[1]
+    c = fastMul(m, e, n)
+    return c
+
+
+def decrypt(c, privateKey):
+    n = privateKey[0]
+    d = privateKey[1]
+    m = fastMul(c, d, n)
+    return m
+
+
+p = 3
+q = 11
+publicKey, privateKey = generateKey(p, q)
+m = int(input("Write your number: "))
+c = encrypt(m, publicKey)
+print("Encrypted text: % s" % c)
+d = decrypt(c, privateKey)
+print("Decrypted text: % s" % d)
